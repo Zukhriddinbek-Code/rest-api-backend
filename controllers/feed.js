e10e1956552b9854c9d95f32ca430b835664eeec
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2024-03-10 18:15:25 +0900

    creating logic for get and post reqs

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,1 +1,15 @@
-exports.getPosts = (req, res) => {};
+exports.getPosts = (req, res) => {
+  res.status(200).json({
+    posts: [{ title: "First post", content: "This the first post" }],
+  });
+};
+
+exports.createPost = (req, res) => {
+  const title = req.body.title;
+  const content = req.body.content;
+  //create post in db
+  res.status(201).json({
+    message: "Post was created successfully!",
+    post: { id: new Date().toISOString(), title: title, content: content },
+  });
+};
--- routes/feed.js
+++ routes/feed.js
@@ -3,6 +3,7 @@ const feedController = require("../controllers/feed");
 
 const router = express.Router();
 
+//feed/posts
 router.get("/posts", feedController.getPosts);
 
 module.exports = router;
