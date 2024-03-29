13b46f5a935f1f44d48c5092617abaa01fc10636
Zukhriddinbek-Code <zuhriddinganiyev2000@gmail.com>
2024-03-30 00:22:02 +0900

    setting up connection with Mongodb

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const feedRoutes = require("./routes/feed");
 
 const app = express();
 
 app.use(bodyParser.json()); //application/json
+
+const MONGODB_URL =
+  "mongodb+srv://zuhriddin_ganiev:8aEZ5BqVm5OVUA4U@cluster-zuhriddin.65mbqpl.mongodb.net/messages?retryWrites=true&w=majority";
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -19,6 +23,11 @@ app.use((req, res, next) => {
 
 app.use("/feed", feedRoutes);
 
-app.listen(8080, () => {
-  console.log("Server is running on 8080");
-});
+mongoose
+  .connect(MONGODB_URL)
+  .then((result) => {
+    app.listen(8080, () => {
+      console.log("Server is running on 8080");
+    });
+  })
+  .catch((err) => console.log(err));
